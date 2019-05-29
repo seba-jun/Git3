96c308329144159598754a298362709f5365bb3e
Nikolai Bravo <nikolaibravos@gmail.com>
2019-05-29 13:08:04 -0400

    changed to haskell file

--- manipulation.hs
+++ manipulation.hs
@@ -1,6 +1,6 @@
 weird xs x = if even x then xs else reverse xs
 
-boustrophedon xss = [ css  | xs <- xss, if length xs `mod` 2 == 0 then xs else reverse xs ]
+boustrophedon xss = [ css  | xs <- xss, if length xs `mod` 2 == 0 then xs else reverse xs ] 
 
 stringSplitter "" = []
 stringSplitter xs = (take 20 xs):(stringSplitter (drop 20 xs))  
